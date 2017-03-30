711b2a17f213c703dd100c04abf8e4d77f5af340
justhis01010101 <1310@gmail.com>
2017-03-30 20:43:43 +0900

    #3 체크박스 이벤트 프로그래밍

--- app/src/main/java/seoyeon/kr/hs/emirim/favoritepet/MainActivity.java
+++ app/src/main/java/seoyeon/kr/hs/emirim/favoritepet/MainActivity.java
@@ -2,12 +2,53 @@ package seoyeon.kr.hs.emirim.favoritepet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+    CheckBox checkselect;
+    TextView textQuewst;
+    RadioGroup rg;
+    RadioButton radioDog, radiofish, radiokol;
+    Button butok;
+    ImageView imgvPet;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkselect=(CheckBox)findViewById(R.id.check_select);
+        rg=(RadioGroup) findViewById(R.id.rg);
+        radioDog=(RadioButton)findViewById(R.id.radio_dog);
+        radiokol=(RadioButton)findViewById(R.id.radio_kol);
+        butok=(Button)findViewById(R.id.but_ok);
+        imgvPet=(ImageView)findViewById(R.id.imgv_pet);
+        checkselect.setOnCheckedChangeListener(this);
+        textQuewst=(TextView)findViewById(R.id.text_quest);
+    }
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if(checkselect.isChecked()){
+            textQuewst.setVisibility(View.VISIBLE);
+            rg.setVisibility(View.VISIBLE);
+            butok.setVisibility(View.VISIBLE);
+            imgvPet.setVisibility(View.VISIBLE);
+        }
+        else{
+            textQuewst.setVisibility(View.INVISIBLE);
+            rg.setVisibility(View.INVISIBLE);
+            butok.setVisibility(View.INVISIBLE);
+            imgvPet.setVisibility(View.INVISIBLE);
+        }
+
     }
 }
